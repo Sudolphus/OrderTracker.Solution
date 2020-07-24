1b1d63eac505bff12337ba785e594c0a2af5ea1c
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-24 13:42:33 -0700

    removed method to view orders independent of vendors, because it didn't make sense

--- OrderTracker/Controllers/OrdersController.cs
+++ OrderTracker/Controllers/OrdersController.cs
@@ -6,17 +6,6 @@ namespace OrderTracker.Controllers
 {
   public class OrdersController : Controller
   {
-    [HttpGet("/orders")]
-    public ActionResult Index()
-    {
-      List<Vendor> vendorList = Vendor.GetAll();
-      List<Order> orderList = Order.GetAll();
-      Dictionary<string, object> listDictionary = new Dictionary<string, object>();
-      listDictionary["vendor"] = vendorList;
-      listDictionary["order"] = orderList;
-      return View(listDictionary);
-    }
-
     [HttpGet("/vendors/{vendorID}/orders/new")]
     public ActionResult New(int vendorID)
     {
