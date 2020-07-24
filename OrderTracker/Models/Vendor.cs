68f3e3cf30f85062281883e9aa66eab4d78ec06e
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-24 12:19:09 -0700

    add method to find a vendor by id

--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -9,12 +9,15 @@ namespace OrderTracker.Models
     private List<Order> _orderList = new List<Order>();
     private static int _nextID = 1;
     private static List<Vendor> _vendorList = new List<Vendor>();
+    private static Dictionary<int, Vendor> _vendorDictionary = new Dictionary<int, Vendor>();
+
     public Vendor(string name)
     {
       Name = name;
       ID = _nextID;
       _nextID++;
       _vendorList.Add(this);
+      _vendorDictionary[ID] = this;
     }
 
     public void AddOrder(Order newOrder)
@@ -40,8 +43,7 @@ namespace OrderTracker.Models
 
     public static Vendor FindVendor(int id)
     {
-      Vendor dummyVendor = new Vendor("dummy");
-      return dummyVendor;
+      return _vendorDictionary[id];
     }
   }
 }
